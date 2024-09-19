152fdcdbccddf3b7a780a464152215c9a5219f4e
abdufattoh1205 <88586375+abdufattoh1205@users.noreply.github.com>
2024-09-19 15:53:53 +0500

    Add files via upload

---
+++ sixth.vue
@@ -0,0 +1,41 @@
+<template>
+    <props bg="title">
+        <h1>{{ title }}</h1>
+    </props>
+
+    <props bg="name">
+        <h1>{{ name }}</h1>
+    </props>
+
+</template>
+
+<script setup>
+
+    import { defineProps } from 'vue';
+
+    defineProps({
+        title: {
+            type: String,
+            default: 'hello world'
+        },
+        name: {
+            type: String,
+            default: 'world'
+        }
+    })
+
+</script>
+
+<style scoped>
+
+/* .title{
+    color: #3795BD;
+}
+
+.name{
+    color: #914F1E;
+} */
+
+
+
+</style>
