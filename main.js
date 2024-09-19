1b6a29079dd53b34c2d0ac196ae84c10a41f7894
abdufattoh1205 <88586375+abdufattoh1205@users.noreply.github.com>
2024-09-19 15:49:50 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,21 @@
-var submit = document.getElementById('submit');
 
-submit.addEventListener('click', (event) =>{
-    event.preventDefault();
+import { createApp } from 'vue'
+import App from './App.vue'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css'
+import './assets/main.css'
+import router from './router';
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var space = document.querySelector('.space')
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BootstrapVue3 } from 'bootstrap-vue-3';
 
-    fetch('http://localhost:3030/api/login', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({username, password})
-    })
-        .then(response => response.json())
-        .then(data => {
-            if(username !== '' && password !== ''){
-                console.log(data)
-                if(data.success){
-                    console.log('success');
-                    localStorage.setItem('token', data.token);
-                    window.location.href = "./welcome.html";
-                }else if(!data.success){
-                    console.log('invalid password or username');
-                    console.error('invalid');
-                    space.style.display = 'block';
-                    space.innerHTML = 'Invalid Password or Username';
-                }    
-            }else{
-                space.style.display = 'block'
-            }
-        })
-        .catch(error => console.error('Error fetching data:', error));
-});
+
+const app = createApp(App)
+
+app.use(BootstrapVue3)
+
+app.use(router)
+app.use(Toast)
+
+app.mount('#app')
