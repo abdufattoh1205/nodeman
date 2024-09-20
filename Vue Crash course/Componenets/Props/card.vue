8c152f05745f2eae1635978c933c9724c91fec1b
abdufattoh1205 <88586375+abdufattoh1205@users.noreply.github.com>
2024-09-20 09:16:06 +0500

    Add files via upload

---
+++ Vue Crash course/Componenets/Props/card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="`${bg}`">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+
+    import { defineProps } from 'vue';
+
+    defineProps({
+        bg:{
+            type: String,
+            default: '#FF8343'
+        }
+    })
+
+</script>
---
+++ Vue Crash course/Componenets/Props/fetch.vue
@@ -0,0 +1,43 @@
+<template>
+    <card bg="redback">
+        <h1>{{ title }}</h1>
+    </card>
+    <card bg="blueback">
+        <h1>{{ bottomTitle }}</h1>
+    </card>
+</template>
+
+<script setup>
+
+import { defineProps } from 'vue';
+import card from './card.vue'
+
+defineProps({
+    title:{
+        type: String,
+        default: 'hello world',
+    },
+    bottomTitle:{
+        type: String,
+        default: 'how is the world',
+    },
+})
+
+</script>
+
+<style scoped>
+
+.redback{
+    background-color: #800000;
+    color: white;
+    padding: 20px;
+}
+
+.blueback{
+    background-color: #1F316F;
+    color: white;
+    margin-left: 20px;
+    padding: 20px;
+}
+
+</style>
